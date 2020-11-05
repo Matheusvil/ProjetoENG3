3313868cdd6db11f1abe0ec91e954a78eb39a69c
unknown <gsilva505@hotmail.com>
2020-11-05 12:39:24 -0300

    atualizando camera(teste foto)

--- frontend/src/components/webcam/index.js
+++ frontend/src/components/webcam/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Webcam from 'react-webcam';
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Button } from 'rsuite';
 import './style.scss';
 
-const WebCam = () =>{
-    return(
-        <Webcam 
-            audio={false}
+const WebcamCapture = () => {
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+  
+    const capture = React.useCallback(() => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+    }, [webcamRef, setImgSrc]);
+  
+    return (
+      <>
+        <Webcam
+          className="camera"
+          width= {400}
+          height={400}
+          audio={false}
+          ref={webcamRef}
+          mirrored="true"
+          screenshotFormat="image/jpeg"
         />
+        <Button appearance="primary" onClick={capture}>Tirar Foto</Button>
+        {imgSrc && (
+          <img
+            src={imgSrc}
+          />
+        )}
+      </>
     );
-};
-
-export default WebCam;
+  };
+  
+export default WebcamCapture;
